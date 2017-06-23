b29ea82521500666f6552034e2d87a8a12fe9344
gaochundong <gaochundong@hotmail.com>
2017-06-23 12:13:11 +0800

    upgrade happer and change self host

--- Sangmado.Fida.Http/HttpServer/InjectedSelfHost.cs
+++ Sangmado.Fida.Http/HttpServer/InjectedSelfHost.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Happer;
+using Happer.Hosting;
 using Happer.Hosting.Self;
 using Sangmado.Inka.Extensions;
 using Sangmado.Inka.Logging;
@@ -14,23 +16,55 @@ namespace Sangmado.Fida.Http
         private ILog _log = Logger.Get<InjectedSelfHost>();
 
         public InjectedSelfHost(IEngine engine, params Uri[] baseUris)
-            : base(engine, baseUris)
+            : base(engine, Environment.ProcessorCount, baseUris)
         {
         }
 
-        protected override async Task Process(HttpListenerContext httpContext)
+        public InjectedSelfHost(IEngine engine, int maxConcurrentNumber, params Uri[] baseUris)
+            : base(engine, maxConcurrentNumber, baseUris)
+        {
+        }
+
+        public InjectedSelfHost(IEngine engine, IRateLimiter rateLimiter, params Uri[] baseUris)
+            : base(engine, rateLimiter, baseUris)
+        {
+        }
+
+        protected override async Task Process(HttpListenerContext httpContext, CancellationToken cancellationToken)
         {
             var watch = Stopwatch.StartNew();
 
-            await base.Process(httpContext);
+            await base.Process(httpContext, cancellationToken);
 
             watch.StopWatch();
             if (watch.ElapsedSeconds() > 0.2)
-                _log.WarnFormat("Process, Url[{0}], StatusCode[{1}], cost [{2}] seconds.",
-                    httpContext.Request.Url, httpContext.Response.StatusCode, watch.ElapsedSeconds());
+                _log.WarnFormat("Request, HttpMethod[{0}], Url[{1}], "
+                    + "UserHostName[{2}], UserHostAddress[{3}], ContentType[{4}], "
+                    + "RemoteEndPoint[{5}], UserAgent[{6}], StatusCode[{7}], "
+                    + "cost [{8}] seconds.",
+                    httpContext.Request.HttpMethod,
+                    httpContext.Request.Url,
+                    httpContext.Request.UserHostName,
+                    httpContext.Request.UserHostAddress,
+                    httpContext.Request.ContentType,
+                    httpContext.Request.RemoteEndPoint,
+                    httpContext.Request.UserAgent,
+                    httpContext.Response.StatusCode,
+                    watch.ElapsedSeconds());
             else
-                _log.DebugFormat("Process, Url[{0}], StatusCode[{1}], cost [{2}] seconds.",
-                    httpContext.Request.Url, httpContext.Response.StatusCode, watch.ElapsedSeconds());
+                _log.DebugFormat("Request, HttpMethod[{0}], Url[{1}], "
+                    + "UserHostName[{2}], UserHostAddress[{3}], ContentType[{4}], "
+                    + "RemoteEndPoint[{5}], UserAgent[{6}], StatusCode[{7}], "
+                    + "cost [{8}] seconds.",
+                    httpContext.Request.HttpMethod,
+                    httpContext.Request.Url,
+                    httpContext.Request.UserHostName,
+                    httpContext.Request.UserHostAddress,
+                    httpContext.Request.ContentType,
+                    httpContext.Request.RemoteEndPoint,
+                    httpContext.Request.UserAgent,
+                    httpContext.Response.StatusCode,
+                    watch.ElapsedSeconds());
         }
     }
 }
